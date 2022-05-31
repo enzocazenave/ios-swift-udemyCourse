3de841e821a2579b05af13ec80b2ea44157bea63
enzocazenave <encaze24@hotmail.com>
2022-05-30 23:52:03 -0300

    Revert "Delete of folder (section 4)"
    This reverts commit 28294dba5c35eb41879739044ab3065dc720487d.

---
+++ CharactersAndStrings-section3.playground/Contents.swift
@@ -0,0 +1,53 @@
+import UIKit
+
+// Definir un string de varios renglones en una variable.
+
+var myString = """
+    Bienvenido a mi playground
+    
+    Mi nombre es Enzo y estas en mi mac.
+    """
+
+
+// Definir un string de varios renglones en una variable, pero solo visualmente en el IDE.
+
+var mySecondString = """
+    Bienvenido a mi playground, \
+    \
+    mi nombre es Enzo y estas en mi mac.
+    """
+
+
+// Caracteres
+
+let myCharacter: Character = "\u{E9}"
+let myCharacter2 = "\u{24}"
+
+
+// Emojis
+
+let myEmoji = "😀"
+
+
+// Interpolacion
+
+let name = "Enzo"
+let country = "Argentina"
+let myPersonalInfo = "Mi nombre es \(name) y vivo en \(country)"
+
+
+// Verificar si en un string aparece una palabra.
+
+let myContent = "Mi nombre es Enzo"
+myContent.contains(name)
+
+
+// Comprobar si un string esta vacio. (myContent == "" [false])
+
+myContent.isEmpty
+
+// Recorrer letras de un string.
+
+for l in myContent {
+    print(l)
+}
