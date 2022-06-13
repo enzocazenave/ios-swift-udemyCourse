e5434b685e5387db7a37598fbc3156077be9669f
enzocazenave <encaze24@hotmail.com>
2022-06-13 16:05:33 -0300

    Modifies CollectionView

---
+++ CollectionViews/CollectionViews/MyCustomCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MyCustomCollectionViewCell.swift
+//  CollectionViews
+//
+//  Created by Enzo Cazenave on 13/06/2022.
+//
+
+import UIKit
+
+class MyCustomCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
